f1343b5dcf28fea531e94523f8d9bba588a2e175
Jose Jose Morgado <jjm30@itl001.student.eecs.qmul.ac.uk>
2017-10-26 19:58:14 +0100

    changes1

--- src/TokenizerMapper.java
+++ src/TokenizerMapper.java
@@ -1,4 +1,20 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> { 
 
-public class TokenizerMapper {
+	private final IntWritable one = new IntWritable(1);
+    private Text data = new Text();
 
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString(), "-- \t\n\r\f,.:;?![]'\"");
+     
+        while (itr.hasMoreTokens()) {
+          data.set(itr.nextToken().toLowerCase());
+          context.write(data, one);
+        }
+    
+    }
 }
